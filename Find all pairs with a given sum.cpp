2bf179fec0b40a8f5ee87a79d43a51d97f1453da
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-05-02 11:31:20 +0530

    Create Find all pairs with a given sum.cpp

---
+++ Find all pairs with a given sum.cpp
@@ -0,0 +1,24 @@
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int a[], int b[], int n, int m, int x)
+    {
+        vector<pair<int,int>>ans;
+        sort(a,a+n);
+        sort(b,b+m);
+        int i=0,j=m-1;
+        while(i<n&&j>=0)
+        {
+            if(a[i]+b[j]==x)
+            {
+                ans.push_back({a[i],b[j]});
+                i++;
+                j--;
+            }
+            else if(a[i]+b[j]<x)
+                i++;
+            else 
+                j--;
+        }
+        return ans;
+    }
+};
