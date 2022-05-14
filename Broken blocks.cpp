1afea555ebef15d41ccb637edefca4310e63843d
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-05-14 07:51:20 +0530

    Create Broken blocks.cpp

---
+++ Broken blocks.cpp
@@ -0,0 +1,31 @@
+class Solution 
+{
+public:
+ int recurse(vector<vector<int>>&matrix,int i,int j,vector<vector<int>>& dp)
+ {
+    if(j<0||j>=matrix[0].size()) 
+       return 0;
+    if(i==matrix.size()) 
+       return 0;
+    if(dp[i][j]!=-1) 
+       return dp[i][j];
+    if(matrix[i][j]==-1)
+        return 0;
+    int ans;
+    //finding to other directions of matrix which one is maximum 
+    //Directions are (straight Down , Down left, Down Right)
+    int a=recurse(matrix,i+1,j,dp);
+    int b=recurse(matrix,i+1,j-1,dp);
+    int c=recurse(matrix,i+1,j+1,dp);
+    return dp[i][j]=matrix[i][j]+max({a,b,c});
+ }
+ int MaxGold(vector<vector<int>>&matrix)
+ {
+    int m=matrix[0].size(),n=matrix.size();
+    vector<vector<int>> dp(n,vector<int>(m,-1));
+    int ans=INT_MIN;
+    for(int j=0;j<m;j++)
+        ans=max(ans,recurse(matrix,0,j,dp));
+    return ans;
+ }
+};
